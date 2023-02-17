320330d22bea089799fbc5fd364e5fd31d8053b8
avtew <avtew@outlook.com>
2023-02-17 16:09:40 +0100

    fix: images paths and others

--- src/js/modules/coaches.js
+++ src/js/modules/coaches.js
@@ -1,12 +1,12 @@
 export const coaches = [
   {
-    "photo": 'url("../img/liudmyla.jpg")',
+    "photo": 'url("img/liudmyla.jpg")',
     "name": 'Людмила Горошкова',
     "rank": 'Кандидат в мастера спорта',
     "about": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima officiis vero culpa totam fugit ab pariatur in porro dolorum possimus, saepe corporis facilis temporibus perferendis deleniti, sequi quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima officiis vero culpa totam fugit ab pariatur in porro dolorum possimus, saepe corporis facilis temporibus perferendis deleniti, sequi quam.',
   },
   {
-    "photo": 'url("../img/liudmyla.jpg")',
+    "photo": 'url("img/liudmyla.jpg")',
     "name": 'Liudmyla Goroshkova',
     "rank": 'Candidate for Master of Sport',
     "about": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima officiis vero culpa totam fugit ab pariatur in porro dolorum possimus, saepe corporis facilis temporibus perferendis deleniti, sequi quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis minima officiis vero culpa totam fugit ab pariatur in porro dolorum possimus, saepe corporis facilis temporibus perferendis deleniti, sequi quam.',
