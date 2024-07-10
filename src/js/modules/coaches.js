cf578e18152337322930e0a97ad045b0ab316f98
Avtew <96073879+avtew@users.noreply.github.com>
2024-07-10 16:26:10 +0200

    Update coaches.js

--- src/js/modules/coaches.js
+++ src/js/modules/coaches.js
@@ -12,13 +12,13 @@ export const coaches = [
     "about": 'He has been teaching chess for over 10 years. In addition to higher pedagogical education, he has higher legal education. She worked in the police, so strict but fair. Multiple participant in various championships. The winner of nominations "The most beautiful participant of the tournament" and "The most beautiful game". He dislikes rapid chess and believes that the real beauty of this game is revealed only in classical carefully thought-out six-hour games. She loves women\'s handbags and her cat Sylvester.',
   },
   {
-    "photo": 'url("../img/maksym.jpg")',
+    "photo": 'url("img/maksym.jpg")',
     "name": 'Максим Горошков',
     "rank": 'Мастер FIDE',
     "about": 'Начинающий, но перспективный тренер. Фристайлер. Берёт от жизни всё, по заветам своего любимого бренда газировки. Чемпион Украины по шахматам в возрастной категории до 8 лет. Чемпион мира по употреблению Pepsi в категории до 18. Любит рапид и блиц. Может играть со скоростью до двух десятков партий в сутки и считает что настоящая красота игры - в импровизации. Обожает футбол. Большой поклонник клуба "Барселона".',
   },
   {
-    "photo": 'url("../img/maksym.jpg")',
+    "photo": 'url("img/maksym.jpg")',
     "name": 'Maksym Goroshkov',
     "rank": 'FIDE Master',
     "about": 'A beginner but promising coach. Freestyler. Takes everything from life, according to the precepts of his favorite soda brand. Champion of Ukraine in chess in the age category up to 8 years. World champion in Pepsi consumption in the category under 18. Likes rapid and blitz. He can play at a speed of up to two dozen games a day and believes that the real beauty of the game is in improvisation. Loves football. Big fan of Barcelona.',
